ee0c08310568396c751b0f4ab365259443e0940f
FelipeAlves99 <falves99.dev@gmail.com>
2021-03-23 23:22:50 -0300

    Added new tracking round variables

--- src/Staticvalues.cs
+++ src/Staticvalues.cs
@@ -1,5 +1,7 @@
 namespace Jogo_Forca.Api{
     public static class StaticValues{
-        public static string word;
+        public static string word = "";
+        public static string newWord = "";
+        public static int? tries;
     }
 }
